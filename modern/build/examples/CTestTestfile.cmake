10edf742bbb4d4c3db8edf99bc6fe3ec77ddef6c
Phil Ratzloff <pratzl@outlook.com>
2025-12-14 13:20:29 -0500

    Remove modern/build/ from git tracking
    Build directories should not be version controlled as they contain
    machine-specific generated files. The build/ pattern in .gitignore
    will prevent them from being re-added.

--- modern/build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phil/dev/boost/libs/graph/modern/examples
-# Build directory: /home/phil/dev/boost/libs/graph/modern/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
